0ae6fdb4ced926fee18e1b02137e20b198c03690
Mikko Lainio <mlainio@gmail.com>
2019-07-05 10:59:40 +0300

    project

---
+++ LanguageParser/LanguageParser/LanguageParser.cpp
@@ -0,0 +1,12 @@
+﻿// LanguageParser.cpp : Defines the entry point for the application.
+//
+
+#include "LanguageParser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
